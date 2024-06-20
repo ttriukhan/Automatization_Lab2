b164880472f72f0b0a215f1de60642ccc562ea96
Tania Triukhan <t.triukhan@ukma.edu.ua>
2024-06-20 05:03:01 +0300

    Added logic

--- main/src/main/java/org/ttriukhan/automatization/Main.java
+++ main/src/main/java/org/ttriukhan/automatization/Main.java
@@ -1,4 +1,16 @@
 package org.ttriukhan.automatization;
 
 public class Main {
+    public static void main(String[] args) {
+        String[] cities = {"Лубни", "Миргород", "Полтава"};
+        String[] streets = {"Миколи Міхновського", "Вербова", "Історична"};
+        for(int i = 0; i < cities.length; i++) {
+            System.out.println("Щоб отримати графік відключень електроенергії по Полтавській області\nВведіть назву населеного пункту: " + cities[i]);
+            System.out.println("Введіть назву вулиці: " + streets[i]);
+            Address address = new Address(streets[i], cities[i]);
+            ScheduleScraper scraper = new ScheduleScraper();
+            Schedule schedule = scraper.getSchedule(address);
+            System.out.println("\n" + schedule.getGroup() + "\nГрафік відключень на сьогодні\n" + schedule.getScheduleString() + "\n\n");
+        }
+    }
 }
