e059261bac2e9689c55d7ed1baa0613ae3718a28
Tania Triukhan <t.triukhan@ukma.edu.ua>
2024-06-20 19:41:36 +0300

    Archetype + archetype project added

---
+++ Archetype/ProjectFromArchetype/core/src/main/java/org/ttriukhan/automatization/Address.java
@@ -0,0 +1,23 @@
+package org.ttriukhan.automatization;
+
+public class Address {
+    private String street;
+    private String city;
+
+    public Address(String street, String city) {
+        this.street = street;
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String toString() {
+        return street + ", " + city;
+    }
+}
